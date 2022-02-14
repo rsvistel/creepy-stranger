cf21387addab2e052009431c58b0081c9d29afa2
Danylo <danyloboshagora@gmail.com>
2022-02-14 23:38:12 +0000

    Styles almost ready

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Dimensions, Pressable, StyleSheet, Text, View, TouchableHighlight, TouchableOpacity} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import {useEffect, useState} from "react";
+
+const screenWidth = Dimensions.get('window').width
 
 export default function App() {
 
@@ -25,6 +27,7 @@ export default function App() {
   const getPercentage = number => counter.total ? Math.round(number / (counter.total / 100)) : 0
 
   const handleUpdate = async (type, subtract) => {
+    if (subtract && counter[type] === 0) return
     const newCounterValues = type === 'total' ?
       {...counter, total: counter.total + (subtract ? -1 : 1)} :
       {...counter, [type]: counter[type] + (subtract ? -1 : 1), total: counter.total + (subtract ? -1 : 1)}
@@ -34,10 +37,10 @@ export default function App() {
   }
 
   const buttonsConfig = [
-    {type: 'dislikes', title: 'Dislike', undoTitle: 'Undo dislike', color: 'red'},
-    {type: 'total', title: 'Boring button', undoTitle: 'Undo boring button', color: 'blue'},
-    {type: 'pretty', title: 'Pretty girl', undoTitle: 'Undo pretty', color: 'green'},
-    {type: 'smash', title: 'So hot!!', undoTitle: 'Undo hot', color: 'orange'}
+    {type: 'dislikes', title: '👎', undoTitle: 'Undo dislike', color: 'red', role: true},
+    {type: 'pretty', title: '🤌', undoTitle: 'Undo pretty', color: 'green', role: false},
+    {type: 'smash', title: '🔥', undoTitle: 'Undo hot', color: 'orange', role: false},
+    {type: 'total', title: 'Boring button', undoTitle: 'Undo boring button', color: 'blue', role: false}
   ]
 
   return (
@@ -46,25 +49,56 @@ export default function App() {
         <Text>Loading...</Text>
       </View> :
       <View style={styles.container}>
-        <Text>
-          Total: {counter.total}{"\n"}
-          Pretty: {getPercentage(counter.pretty)}% {counter.pretty}{"\n"}
-          Smash: {getPercentage(counter.smash)}% {counter.smash}{"\n"}
-          Dislikes: {getPercentage(counter.dislikes)}% {counter.dislikes}{"\n"}
-        </Text>
-
-        {buttonsConfig.map(button => (
-          <View key={button.type}>
-            <Button
-              onPress={() => handleUpdate(button.type)}
-              title={button.title}
-              color={button.color}/>
-            <Button
-              onPress={() => handleUpdate(button.type,true)}
-              title={button.undoTitle}
-              color={button.color}/>
+        <View style={styles.dataWrapper}>
+          <Text style={styles.dataText}>
+            {counter.total}{"\n"}
+            Pretty: {getPercentage(counter.pretty)}% {counter.pretty}{"\n"}
+            Smash: {getPercentage(counter.smash)}% {counter.smash}{"\n"}
+            Dislikes: {getPercentage(counter.dislikes)}% {counter.dislikes}{"\n"}
+          </Text>
+          <Text style={styles.totalCounter}>{counter.total}</Text>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: screenWidth * 0.68
+          }}>
+            {['dislikes', 'smash', 'pretty'].map(type => (
+                <TouchableOpacity
+                    key={type}
+                    style={{alignItems: 'center'}}
+                    onPress={() => handleUpdate(type,true)}>
+                  <View style={{flexDirection: "row", alignItems: 'flex-end'}}>
+                    <Text style={[styles.dataCounter,
+                      {fontSize: type === 'smash' ? 50 : 32, color: type === 'dislikes' ? 'coral' : '#fff'}
+                    ]}>
+                      {getPercentage(counter[type])}
+                    </Text>
+                    <Text style={[styles.percentSymbol,
+                      {lineHeight: type === 'smash' ? 38 : 32, color: type === 'dislikes' ? 'coral' : '#fff'}
+                    ]}>%</Text>
+                  </View>
+                  <Text style={[styles.dataQuantity, {color: type === 'dislikes' ? 'coral' : '#fff'}]}>{counter[type]}</Text>
+                </TouchableOpacity>
+            ))}
           </View>
-        ))}
+        </View>
+
+        <View style={styles.buttonsWrapper}>
+          {buttonsConfig.map(button => (
+            <View key={button.type}>
+              <TouchableHighlight
+                onPress={() => handleUpdate(button.type)}
+                style={[
+                    styles.mainButton,
+                    {width: button.type === 'total' ? screenWidth * 0.68 : screenWidth * 0.2}
+                ]}
+                color={button.color}>
+                <Text style={styles.buttonText}>{button.title}</Text>
+              </TouchableHighlight>
+            </View>
+          ))}
+        </View>
       </View>
   );
 }
@@ -72,8 +106,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: screenWidth * 0.16
   },
+
+  dataWrapper: {
+    marginTop: screenWidth * 0.3
+  },
+
+  dataText: {
+    color: '#fff'
+  },
+
+  totalCounter: {
+    color: '#fff',
+    fontSize: 72,
+    fontWeight: '700'
+  },
+
+  dataCounter: {
+    fontWeight: '700',
+    fontSize: 32,
+    color: '#fff'
+  },
+
+  dataQuantity: {
+    color: '#fff',
+    fontSize: 16
+  },
+
+  percentSymbol: {
+    fontSize: 18,
+    lineHeight: 32,
+    color: '#fff',
+    fontWeight: '700'
+  },
+
+  mainButton: {
+    borderRadius: 20,
+    backgroundColor: '#242424',
+    height: screenWidth * 0.2,
+    justifyContent: 'center',
+    marginBottom: screenWidth * 0.04
+  },
+
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#fff'
+  },
+
+  buttonsWrapper: {
+    flexDirection: "row",
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginBottom: screenWidth * 0.1
+  }
 });
